129e72d4c3423936cbd69d4659111a0c7396d3d2
dway123 <huangdarwin2@gmail.com>
2016-04-23 22:29:56 -0400

    Changed Game.java
    Updated game.java to increase modularity of code and make it easier to do some GUI coding without trudging through too much unnecessary code
    
    Update SetGameLogic.java to support/test 3 players

--- Game.java
+++ Game.java
@@ -9,28 +9,80 @@ import java.util.Scanner;
 public class Game {
 	private ArrayList<Player> players;
 	private Table t;
+	int[] playerScores; //represents current scores of players
 	
 	//The constructor creates a game with the
-	//specified amount of players and a 
+	//input array of players who want to play
 	public Game(ArrayList<Player> playersIn){
 		players = playersIn;
 		t = new Table();
+		playerScores = new int[players.size()]; //represents current scores of players
 	}
 	
-	public void play(){
-		//Setup
-		int playerToMove;
-		int cardIndex1;
-		int cardIndex2;
-		int cardIndex3;
+	//move takes in the player moving, as well as their three choices of cards
+	//increments points if the set is correct, decrements points if the set is incorrect
+	//also updates the table and deck to prepare for the next turn
+	//returns 0 if the move is incorrect, 1 if it is correct
+	public boolean move(int playerToMove, int cardIndex1, int cardIndex2, int cardIndex3){
 		Card card1;
 		Card card2;
 		Card card3;
+		if(t.isSet(cardIndex1, cardIndex2, cardIndex3)){ //If correct, +1 to score, remove and replace cards from table
+			playerScores[playerToMove]++;
+			card1 = t.getCard(cardIndex1);
+			card2 = t.getCard(cardIndex2);
+			card3 = t.getCard(cardIndex3);
+			t.removeCard(card1);
+			t.removeCard(card2);
+			t.removeCard(card3);
+			if(t.getCardCount() < 12){ //only draws more cards if there are fewer than 12 on the table
+				t.addCard(3);
+			}
+			
+			//add cards to table until set exists or no cards in deck
+			while(!t.setsExist() && (t.getDeckCardCount() > 0)){
+				t.addCard(3);
+			}
+			return true;
+		}
+		else{ //If incorrect -1 to score, no change to table
+			playerScores[playerToMove]--;
+			return false;
+		}		
+	}
+	
+	//Finds the index of the player with the most points
+	public int mostPointsPlayerIndex(){
+		int mostIndex = 0;
+		for(int i = 0; i < playerScores.length; i++){
+			if(playerScores[i] > playerScores[mostIndex]){
+				mostIndex = i;
+			}
+		}
+		return mostIndex;
+	}
+	
+	public void updateHighScores(){
+		for(int i = 0; i < playerScores.length; i++){
+			if(playerScores[i] > players.get(i).getHighScore()){
+				players.get(i).setHighScore(playerScores[i]);
+			}
+		}
+	}
+	
+	//The main function in Game, provides a fun textual experience with playing Set.
+	public void play(){
+		//Setup
+		int playerToMove;
+		boolean moveCorrect;
+		int cardIndex1;
+		int cardIndex2;
+		int cardIndex3;
 		Scanner gameReader = new Scanner(System.in);
-		int[] playerScores = new int[players.size()]; //represents current scores of players
+		
 		
 		//Game introduction
-		System.out.println("Welcome to the Set Game");
+		System.out.println("Welcome to the Set Game!!");
 		System.out.println("Today's players are: ");
 		//Print participating players
 		for(int i = 0; i < players.size(); i++){
@@ -44,7 +96,7 @@ public class Game {
 			System.out.println(t);
 			t.showAllSets(); //SHOW ALL SETS FOR TESTING ONLY!
 			
-			//Wait for a player to move
+			//Enter a move (player number)
 			System.out.println("To select a set, please enter the player number:");
 			
 			playerToMove = gameReader.nextInt();
@@ -54,7 +106,7 @@ public class Game {
 				continue; //try again
 			}
 			
-			//Accept a move
+			//Enter a move (card indices)
 			System.out.println("Select the first card index: ");
 			cardIndex1 = gameReader.nextInt();
 			System.out.println("Select the second card index: ");
@@ -68,57 +120,39 @@ public class Game {
 				continue;
 			}
 			
-			
-			//Determine validity of Set and react appropriately
-			if(t.isSet(cardIndex1, cardIndex2, cardIndex3)){ //If correct, +1 to score, remove and replace cards from table
-				playerScores[playerToMove]++;
-				card1 = t.getCard(cardIndex1);
-				card2 = t.getCard(cardIndex2);
-				card3 = t.getCard(cardIndex3);
-				t.removeCard(card1);
-				t.removeCard(card2);
-				t.removeCard(card3);
-				if(t.getCardCount() < 12){ //only draws more cards if there are fewer than 12 on the table
-					t.addCard(3);
-				}
+			//Execute move: Determine validity of Set and react appropriately
+			moveCorrect = move(playerToMove, cardIndex1, cardIndex2, cardIndex3);
+			if(moveCorrect){
 				System.out.println(players.get(playerToMove).getName() + " has discovered a valid set, and now has " + playerScores[playerToMove] + " points.");
-				
-				//add cards to table until set exists or no cards in deck
-				while(!t.setsExist() && (t.getDeckCardCount() > 0)){
-					t.addCard(3);
-				}
-				//if sets exist and no cards in deck... game over!
-				if(!t.setsExist()){
-					System.out.println(t);
-					break;
-				}
 			}
-			else{ //If incorrect -1 to score, no change to table
-				playerScores[playerToMove]--;
+			else{
 				System.out.println(players.get(playerToMove).getName() + " has selected an invalid set, and now has " + playerScores[playerToMove] + " points.");
 			}
-		}
-		//detect winner index (max score) (doesn't work for ties!)
-		int winnerIndex = 0;
-		for(int i = 0; i < playerScores.length; i++){
-			if(playerScores[i] > playerScores[winnerIndex]){
-				winnerIndex = i;
+			
+			//Check for game over
+			if(!t.setsExist()){
+				System.out.println(t);
+				break;
 			}
 		}
-		
 		//Game Over!
+		
+		//detect winner index (max score) (doesn't work well for ties!)
+		
+		int winnerIndex = mostPointsPlayerIndex();
 		System.out.println("GAME OVER!");
 		System.out.println("The winner is " + players.get(winnerIndex).getName() + ", with " + playerScores[winnerIndex] + " points!");
 		System.out.println();
 		
-		//Print final scores and update high scores
+		//Print final scores
 		System.out.println("Final scores: ");
 		for(int i = 0; i < playerScores.length; i++){
 			System.out.println(players.get(i).getName() + ": " + playerScores[i]);
-			if(playerScores[i] > players.get(i).getHighScore()){
-				players.get(i).setHighScore(playerScores[i]);
-			}
 		}
+		
+		//Update high scores
+		updateHighScores();
+		
 		gameReader.close();
 	}	
 }
--- SetGameLogic.java
+++ SetGameLogic.java
@@ -12,10 +12,12 @@ public class SetGameLogic {
 	public static void main(String[] args) {
 		Player p1 = new Player("Player 1");
 		Player p2 = new Player("Player 2");
+		Player p3 = new Player("Player 3");
 		
 		ArrayList<Player> players = new ArrayList<Player>();
 		players.add(p1);
 		players.add(p2);
+		players.add(p3);
 		
 		Game g = new Game(players);
 		g.play();
